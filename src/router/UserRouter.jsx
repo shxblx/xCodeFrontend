2753ede97289dcebc8107310b86b016a4deb4b8d
shxblx <shiblibasheer27@gmail.com>
2025-01-31 05:55:39 +0530

    typo fix

--- src/router/UserRouter.jsx
+++ src/router/UserRouter.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import SignUp from "../components/Signup.jsx";
+import Signup from "../components/Signup.jsx"; 
 import { Route, Routes } from "react-router-dom";
 import TaskManagement from "../components/TaskManager.jsx";
 
 const UserRouter = () => {
   return (
     <Routes>
-      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/home" element={<TaskManagement />} />
     </Routes>
   );
